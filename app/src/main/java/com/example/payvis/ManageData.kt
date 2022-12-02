0c22630cd21869c2984aa713a1ecb69676bd4dfa
self <adrian.villegas173@gmail.com>
2022-12-02 16:57:41 -0600

    Migrated export data code and created a notification text view for manage data activity

--- app/src/main/java/com/example/payvis/ManageData.kt
+++ app/src/main/java/com/example/payvis/ManageData.kt
@@ -1,10 +1,34 @@
 package com.example.payvis
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
 
 class ManageData: AppCompatActivity() {
+
+    fun vibrate(milliSec: Long = 50, effect: Int = VibrationEffect.DEFAULT_AMPLITUDE){
+        // Vibrates for a specified amount of milliseconds.
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        if (vibrator.hasVibrator()){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {vibrator.vibrate(
+                VibrationEffect.
+            createOneShot(milliSec, effect))}
+            else {
+                vibrator.vibrate(milliSec)
+            }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +39,8 @@ class ManageData: AppCompatActivity() {
         val exportDataButton = findViewById<Button>(R.id.export_data_button)
         val loadDataButton = findViewById<Button>(R.id.load_data_button)
 
+        val notifyView = findViewById<TextView>(R.id.notify_view2)
+
         // Create listeners ============
         // Back
         backButton.setOnClickListener {
@@ -24,6 +50,30 @@ class ManageData: AppCompatActivity() {
 
         // Export Data
         exportDataButton.setOnClickListener {
+            vibrate()
+            var uri: Uri
+            val intent = Intent(Intent.ACTION_SEND)
+
+            try{
+                // 1. Get uri
+                if (File(applicationContext.filesDir, "workDB.json").exists()){
+                    uri = FileProvider.getUriForFile(
+                        this@ManageData,
+                        "com.example.payvis.MainActivity",
+                        File(applicationContext.filesDir, "workDB.json")
+                    )
+
+                    // 2. Share
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    intent.setType("*/*")
+                    intent.putExtra(Intent.EXTRA_STREAM, uri)
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+            } catch (t: Throwable){
+                notifyView.text = "Unable to export file: $t"
+                println("[X] Failed to export file: $t")
+            }
 
         }
 
@@ -32,11 +82,5 @@ class ManageData: AppCompatActivity() {
         loadDataButton.setOnClickListener {
 
         }
-
-
-
-
-
     }
-
 }
